993a8c63b3bcdfb605d4314672ed4489a246791c
AlexAvila <alexavila.oc@gmail.com>
2020-01-24 18:05:16 -0800

    setup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        PRODUCT DETAIL PAGE
+      </div>
+    );
+  }
+}
+
+export default App;
