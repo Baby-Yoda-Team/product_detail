f3cb08221d58ccf338c812fff56e40ddc7c9ac52
AlexAvila <alexavila.oc@gmail.com>
2020-01-28 10:03:39 -0800

    faker data examples

---
+++ src/fakerSampleData.js
@@ -0,0 +1,38 @@
+// you need to require faker
+var faker = require('faker');
+
+
+const sampleData = [{
+
+  id: 1,
+  productName: faker.commerce.productName(),
+  productItemNumber: faker.finance.account(),
+  productPrice: '$ ' + faker.commerce.price(),
+  productFeatures: {
+    stringOne: 'Made out of the best ' + faker.commerce.productMaterial().toLowerCase() + ' in the market',
+    stringTwo: faker.company.catchPhrase(),
+    stringThree: faker.company.bsBuzz(),
+    stringFour: faker.lorem.sentence(),
+    stringFive: faker.commerce.product() + ' not included.'
+  }
+}]
+
+// console.log(sampleData)
+console.log(sampleData[0]['productName'])
+
+// for(var i =0; i < 100; i++){
+//   console.log({
+//     id:i,
+//     productName:faker.commerce.productName(),
+//     productItemNumber: faker.finance.account(),
+//     productPrice: '$ ' + faker.commerce.price(),
+//     productFeatures: {
+//       stringOne:'Made out of the best ' + faker.commerce.productMaterial().toLowerCase() + ' in the market',
+//       stringTwo:faker.company.catchPhrase(),
+//       stringThree:faker.company.bsBuzz(),
+//       stringFour:faker.lorem.sentence(),
+//       stringFive:faker.commerce.product() + ' not included.'
+//     }
+
+//   })
+// }
