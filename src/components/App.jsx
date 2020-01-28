7c3dd5c53803c5694530c2b6e93270b47f84950f
AlexAvila <alexavila.oc@gmail.com>
2020-01-28 10:04:37 -0800

    simplify divs on APP.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,31 +5,36 @@ class App extends Component {
     return (
       <div>
         <div class="top">
-          <h1 itemprop="name">NordicTrack Z 1300i Treadmill with 1-Year iFit Coach Included- Assembly Required</h1>
-          <span class="bv-rating-stars-on bv-rating-stars bv-width-from-rating-stats-78" aria-hidden="true">  ★★★★★  </span>
-          <span itemprop="ratingValue" aria-hidden="true">3.9</span>
-          <span itemprop="reviewCount">(366)</span>
-          <p id="product-body-item-number" class="item-number ">Item &nbsp;<span data-sku="1312119">1312119</span></p>
+          <h1 class="name">NordicTrack Elite 1400 Treadmill with 1-Year iFit Coach Membership- Assembly Required</h1>
+          <span class="stars-rating">  ★★★★★  </span>
+          <span class="ratingValue">3.0</span>
+          <span class="reviewCount">(366)</span>
+          <p>Item &nbsp;<span> 1312119</span></p>
         </div>
 
-        <div class="online-price active" data-opvalue="MSwyOTkuOTk=" data-catentry="1476596" data-inv="IN_STOCK">
+        <div class="your-price">
+          <span class="text">Your Price </span>
+          <span class="value">$1,999.99</span>
+	      </div>
+
+
+        {/* <div class="online-price active">
           <span class="op-label">Online Price</span>
           <span class="currency">$</span>
           <span class="op-value">1,299.99</span>
-        </div>
-
-        <div class="disc active" data-disc="NTAwLjAw" data-catentry="1476596">
+        </div> */}
+        {/* <div class="disc active" data-disc="NTAwLjAw" data-catentry="1476596">
           <span class="disc-label">Less</span>
           <span class="minus-ca-fr">-</span>
           <span class="minus">-</span>
           <span class="currency">$</span>
           <span class="disc-value">500.00</span>
-        </div>
+        </div> */}
 
-        <div class="features-container form-group">
+        <div class="features-container">
           <p id="shipping-statement">Shipping &amp; Handling Included</p>
           <span>Features:</span>
-            <ul class="pdp-features">
+            <ul class="product-features">
               <li>1-Year iFit® Coach Membership Included</li>
               <li> Custom-Focus Weight Loss Workouts with Weight Entry</li>
               <li>3.0 CHP Motor</li>
@@ -38,19 +43,28 @@ class App extends Component {
             </ul>
         </div>
 
-      <div class="form-group">
-        <input type="button" id="add-to-cart-btn" value="Add to Cart" class="primary" name="add-to-cart"/>
+
+        <div class="form-container">
+				 <ul>
+				 		<li id="qty-input">
+				      <label for="minQtyText" class="offscreen">Qty</label>
+								<input type="tel" id="minQtyText" class="form-control valid" name="quantity" maxlength="3" value="1"/>
+				    </li>
+
+            <li id="add-to-cart">
+              <input type="button" id="add-to-cart-btn" value="Add to Cart" class="primary" name="add-to-cart"/>
+            </li>
+				</ul>
 			</div>
 
+
       <div class="style-check compare-container">
-        <input id="compare_100467947" type="checkbox" name="compare-product" value="true" data-description="NordicTrack Z 1300i Treadmill with 1-Year iFit Coach Included- Assembly Required" title="Compare Product" onclick="COSTCO.compareProducts.toggleItem('100467947','https://images.costco-static.com/ImageDelivery/imageService?profileId=12026540&amp;imageId=100467947-847__1&amp;recipeName=350', this)"/>
-        <label for="compare_100467947" title="Compare Product" class="mar_top">
+        <input id="compare" type="checkbox" name="compare-product" value="true" />
           <span>Compare Product</span>
-        </label>
       </div>
 
 
-      <span class="social-share-label visible-xs pull-left">Share</span>
+      <span class="social-media">Share</span>
 	      <ul class="social list-inline">
           <li class="social-icon" onclick="COSTCO.Social.shareWindow('facebook')">
             <label class="hide">facebook</label>
@@ -69,7 +83,6 @@ class App extends Component {
             <i class="fi-social-google-plus"></i>
           </li>
         </ul>
-    	<span class="social-share-label hidden-xs pull-right">Share</span>
 
       </div>
     );
