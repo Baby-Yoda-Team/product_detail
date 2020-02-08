ac5e34b0483b6b9e5d82fbbc8a00a71a65ca0187
AlexAvila <alexavila.oc@gmail.com>
2020-02-08 14:14:01 -0800

    change the ec2 call

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
 const port = 3008;
+const EC = 'http://ec2-52-14-25-174.us-east-2.compute.amazonaws.com:3008/'
 
 const fetchData = function(productId){
-  return axios.get(`http://${window.location.hostname}:${port}/api/details?productId=${productId}` )
+  // return axios.get(`http://${window.location.hostname}:${port}/api/details?productId=${productId}` )
+  return axios.get(`${EC}api/details?productId=${productId}` )
   .then((data) => {
     // console.log('here we have all the Product Details', data)
     return data;
